8e0d11c1045298bf42ccbe7a6fb3da0985f8aba6
Starignus <ariadna@starignus.com>
2015-09-29 20:32:54 +0100

    csv data and server

---
+++ Scatterplot/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import flask
+
+# Creation of little application
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+  return flask.render_template('index.html')     
+
+# This line fires up the server if we want to run that file as a standalone application
+if __name__ == '__main__':
+    app.run(debug=True)  
